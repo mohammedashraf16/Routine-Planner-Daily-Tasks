b6c725b1a4635a7201b081044b32c0e0697acea7
mohammedashraf16 <mohammedashraf1692003@gmail.com>
2024-07-25 22:23:06 +0300

    Finished home screen ui. Add bottom Sheet for add task. Add list view of tasks. Add Animated Text Kit.

---
+++ lib/features/home/tabs/tasks_tab.dart
@@ -0,0 +1,47 @@
+import 'package:calendar_timeline/calendar_timeline.dart';
+import 'package:flutter/material.dart';
+import 'package:todo_app/app_colors.dart';
+import 'package:todo_app/features/widgets/task_item.dart';
+
+class TasksTap extends StatelessWidget {
+  const TasksTap({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 140,
+          decoration: BoxDecoration(
+            color: AppColors.primaryColor
+          ),
+          child: CalendarTimeline(
+            initialDate: DateTime.now(),
+            firstDate: DateTime.now().subtract(const Duration(days: 365*2)),
+            lastDate:DateTime.now().add(const Duration(days: 365*2)),
+            onDateSelected: (date) => print(date),
+            leftMargin: 20,
+            monthColor: AppColors.blackColor,
+            dayColor: AppColors.blackColor,
+            activeDayColor: AppColors.whiteColor,
+            activeBackgroundDayColor: Colors.white38,
+            dotColor: AppColors.whiteColor,
+            selectableDayPredicate: (date) => date.day != 23,
+            locale: 'en',
+          ),
+        ),
+       const SizedBox(height: 20),
+       Expanded(
+         child: ListView.separated(
+           separatorBuilder: (context, index) {
+             return const SizedBox(height: 20);
+           },
+           itemCount: 20,
+           itemBuilder: (context, index) {
+           return const TaskItem();
+         },),
+       )
+      ],
+    );
+  }
+}
