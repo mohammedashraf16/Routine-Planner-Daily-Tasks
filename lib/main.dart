57b68d01f7c257f8aacbb120f765887ccf9e1db2
mohammedashraf16 <mohammedashraf1692003@gmail.com>
2024-08-05 13:40:22 +0300

    connect the app to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app/features/home/home_screen.dart';
+import 'package:todo_app/firebase_options.dart';
 import 'package:todo_app/providers/app_config_provider.dart';
 import 'package:todo_app/theme/my_theme_data.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-void main() {
-  runApp(ChangeNotifierProvider(
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+      ChangeNotifierProvider(
     create: (context) => AppConfigProvider()..getTheme()..getLanguage(),
       child: const MyApp()));
 }
